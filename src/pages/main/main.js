9c050f0d3920482eb608e824dbc775b406003d58
Dirceu de Souza Rezende <dirceu.srezende@gmail.com>
2021-02-16 19:31:49 -0300

    Adicionado as páginas Orders, PizzaFlavours, Pizzas Sizes. M4#A180

--- src/pages/main/main.js
+++ src/pages/main/main.js
@@ -1,17 +1,103 @@
-import React, { Suspense } from 'react'
-import { Switch, Route } from 'react-router-dom'
+import React, { Suspense, lazy } from 'react'
+import styled from 'styled-components'
+import { Link, Route, Switch, useLocation } from 'react-router-dom'
+import {
+  Divider,
+  Drawer as MaterialDrawer,
+  List,
+  ListItem,
+  ListItemText,
+  Typography
+} from '@material-ui/core'
 
-const Main = () => (
-  <>
-    <Suspense fallback={<h1>Loading...</h1>}>
-      <Switch>
-        <Route>
-          <h1>main</h1>
-        </Route>
-      </Switch>
-    </Suspense>
+import * as routes from 'routes'
 
-  </>
-)
+const Orders = lazy(() => import('pages/orders'))
+const PizzasSizes = lazy(() => import('pages/pizzas-sizes'))
+const PizzasFlavours = lazy(() => import('pages/pizzas-flavours'))
+
+const Main = () => {
+  const { pathname } = useLocation()
+
+  return (
+    <h1>
+      <Drawer variant='permanent'>
+        <DrawerContent>
+          <Typography variant='h4'>
+            React-zzaria
+          </Typography>
+
+          <Typography>
+            (Sistema de cadastro)
+          </Typography>
+        </DrawerContent>
+
+        <Divider />
+
+        <List>
+          {menuItems.map(item => (
+            <ListItem key={item.label} button component={Link} to={item.link} selected={pathname === item.link}>
+              <ListItemText>
+                {item.label}
+              </ListItemText>
+            </ListItem>
+          ))}
+        </List>
+
+      </Drawer>
+      <Content>
+        <Suspense fallback={<h1>Loading...</h1>}>
+          <Switch>
+            {menuItems.map(item => (
+              <Route key={item.link} path={item.link} exact={item.exact}>
+                <item.component />
+              </Route>
+            ))}
+          </Switch>
+        </Suspense>
+      </Content>
+
+    </h1>
+  )
+}
+
+const menuItems = [
+  {
+    label: 'Pedidos',
+    link: routes.HOME,
+    component: Orders,
+    exact: true
+  },
+  {
+    label: 'Tamanhos de pizzas',
+    link: routes.PIZZAS_SIZES,
+    component: PizzasSizes
+  },
+  {
+    label: 'Sabores de pizzas',
+    link: routes.PIZZAS_FLAVOURS,
+    component: PizzasFlavours
+  }
+]
+
+const Drawer = styled(MaterialDrawer)`
+  && {
+    .MuiPaper-root {
+      width: ${({ theme }) => theme.extend.drawerWidth}px;
+    }
+  }
+`
+
+const DrawerContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: ${({ theme }) => theme.spacing(1)}px;
+  text-align: center;
+`
+
+const Content = styled.main`
+  margin-left: ${({ theme }) => theme.extend.drawerWidth}px;
+  padding: ${({ theme }) => theme.spacing(3)}px;
+`
 
 export default Main
---
+++ src/pages/pizzas-flavours/pizzas-flavours.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const PizzasFlavours = () => {
+  return (
+    <h1>Sabores de Pizzas</h1>
+  )
+}
+
+export default PizzasFlavours
